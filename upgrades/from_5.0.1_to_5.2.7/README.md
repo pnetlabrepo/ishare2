8b1202ec3a5df87cb8439bed4926b864cc22b210
pnetlabrepo <pnetlabimages1@gmail.com>
2023-01-10 05:17:52 -0300

    Added folder 5.0.1 to 5.2.7

---
+++ upgrades/from_5.0.1_to_5.2.7/README.md
@@ -0,0 +1,24 @@
+```linux
+This script allows to upgrade PNETLab from v5.0.1 to v5.2.7
+```
+## upgrade.sh
+
+`Only applicable for cases where your PNETLab version is 5.0.1. Otherwise, an error message will be displayed and the script execution will be finished`
+
+`Also, this script detects if you already have PNETLab v5.2.7 in your system and avoids to upgrade twice`
+
+### Upgrade
+Apply this command using the terminal
+```linux
+wget -O upgrade.sh https://raw.githubusercontent.com/pnetlabrepo/ishare2/main/upgrades/from_5.0.1_to_5.2.7/upgrade.sh > /dev/null 2>&1 && chmod +x upgrade.sh && bash upgrade.sh
+```
+
+After a few seconds, the upgrade will be done.
+
+Note: This upgrade does not use ishare2:
+
+When you execute the command from above, the steps are as follows:
+
+1) A file called [upgrade.sh](https://raw.githubusercontent.com/pnetlabrepo/ishare2/main/upgrades/from_5.0.1_to_5.2.7/upgrade.sh) will be downloaded from this repository
+2) Execution permissions will be made to this file
+3) This script will be executed in order to make the upgrade
---
+++ upgrades/from_5.0.1_to_5.2.7/upgrade.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Script designed to upgrade PNETLab from 5.0.1 to NEW_PNETLAB_VERSION version
+# Requirement: Having PNETLab 5.0.1 installed for being able to upgrade it to NEW_PNETLAB_VERSION
+# This script avoids to make this upgrade process twice
+
+NEW_PNETLAB_VERSION=5.2.7
+
+data=$(mysql -uroot -ppnetlab -D pnetlab_db -e "SELECT control_value FROM control WHERE control_value>1;" 2>/dev/null)
+pnetlab_info=($data)
+pnetlab_version=${pnetlab_info[1]}
+
+if [[ $pnetlab_version == $NEW_PNETLAB_VERSION ]]; then
+    echo "PNETLab already upgraded to v$NEW_PNETLAB_VERSION"
+    exit 0
+fi
+
+if ! [[ $pnetlab_version == "5.0.1" ]]; then
+    echo "You need to have the v5.0.1 to upgrade to the v$NEW_PNETLAB_VERSION"
+    exit 0
+fi
+
+GREEN='\033[32m'
+NO_COLOR='\033[0m'
+    
+echo -e "${GREEN}Downloading $NEW_PNETLAB_VERSION zip file...${NO_COLOR}"
+wget -q --show-progress -O /root/$NEW_PNETLAB_VERSION.zip https://raw.githubusercontent.com/pnetlabrepo/ishare2/main/upgrades/from_5.0.1_to_$NEW_PNETLAB_VERSION/$NEW_PNETLAB_VERSION.zip
+echo -e "${GREEN}$NEW_PNETLAB_VERSION zip file has been downloaded successfully${NO_COLOR}"
+cd /root
+rm -rf upgrade
+unzip $NEW_PNETLAB_VERSION.zip -d ./upgrade > /dev/null 2>&1
+chmod 755 -R upgrade
+find upgrade -type f -print0 | xargs -0 dos2unix > /dev/null 2>&1
+echo -e "${GREEN}Upgrading to v$NEW_PNETLAB_VERSION...${NO_COLOR}"
+./upgrade/upgrade
+echo -e "${GREEN}Upgrade to v$NEW_PNETLAB_VERSION has been done successfully${NO_COLOR}"
+rm -rf upgrade
+#rm $NEW_PNETLAB_VERSION.zip
+echo -e "${GREEN}PNETLab VM will be rebooted now${NO_COLOR}"
+reboot
