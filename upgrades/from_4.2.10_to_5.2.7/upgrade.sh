3064fdcab23dff6fbffcf85a97a99909b171b640
pnetlabrepo <115958905+pnetlabrepo@users.noreply.github.com>
2023-01-18 02:19:38 -0300

    Update upgrade.sh

--- upgrades/from_4.2.10_to_5.2.7/upgrade.sh
+++ upgrades/from_4.2.10_to_5.2.7/upgrade.sh
@@ -4,8 +4,13 @@
 # Requirement: Having PNETLab 4.2.10 installed for being able to upgrade it to NEW_PNETLAB_VERSION
 # This script avoids to make this upgrade process twice
 
+# CONSTANTS
 NEW_PNETLAB_VERSION=5.2.7
+GREEN='\033[32m'
+NO_COLOR='\033[0m'
+URL_ZIP_FILE=https://raw.githubusercontent.com/pnetlabrepo/ishare2/main/upgrades/from_4.2.10_to_$NEW_PNETLAB_VERSION/$NEW_PNETLAB_VERSION.zip
 
+# Getting PNETLab version from db
 data=$(mysql -uroot -ppnetlab -D pnetlab_db -e "SELECT control_value FROM control WHERE control_value>1;" 2>/dev/null)
 pnetlab_info=($data)
 pnetlab_version=${pnetlab_info[1]}
@@ -19,22 +24,27 @@ if ! [[ $pnetlab_version == "4.2.10" ]]; then
     echo "You need to have the v4.2.10 to upgrade to the v$NEW_PNETLAB_VERSION"
     exit 0
 fi
-
-GREEN='\033[32m'
-NO_COLOR='\033[0m'
     
 echo -e "${GREEN}Downloading $NEW_PNETLAB_VERSION zip file...${NO_COLOR}"
-wget -q --show-progress -O /root/$NEW_PNETLAB_VERSION.zip https://raw.githubusercontent.com/pnetlabrepo/ishare2/main/upgrades/from_4.2.10_to_$NEW_PNETLAB_VERSION/$NEW_PNETLAB_VERSION.zip
+wget -q --show-progress -O /root/$NEW_PNETLAB_VERSION.zip $URL_ZIP_FILE
 echo -e "${GREEN}$NEW_PNETLAB_VERSION zip file has been downloaded successfully${NO_COLOR}"
-cd /root
-rm -rf upgrade
+
+cd /root && rm -rf upgrade
+
+echo -e "${GREEN}Unzipping zip file...${NO_COLOR}"
 unzip $NEW_PNETLAB_VERSION.zip -d ./upgrade > /dev/null 2>&1
+echo -e "${GREEN}Zip file has been unzipped successfully...${NO_COLOR}"
+
 chmod 755 -R upgrade
 find upgrade -type f -print0 | xargs -0 dos2unix > /dev/null 2>&1
+
 echo -e "${GREEN}Upgrading to v$NEW_PNETLAB_VERSION...${NO_COLOR}"
 ./upgrade/upgrade
 echo -e "${GREEN}Upgrade to v$NEW_PNETLAB_VERSION has been done successfully${NO_COLOR}"
+
 rm -rf upgrade
 #rm $NEW_PNETLAB_VERSION.zip
-echo -e "${GREEN}PNETLab VM will be rebooted now${NO_COLOR}"
+
+echo -e "${GREEN}PNETLab VM will be rebooted right now...${NO_COLOR}"
+echo -e "${GREEN}Try to connect again in about a minute${NO_COLOR}"
 reboot
