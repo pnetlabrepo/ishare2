6d773c6b3123d95425f8980e1ddec4018c12a85f
Alex <alex@alex-dev.me>
2023-03-05 20:06:59 -0600

    Fixed broken links

--- upgrades/from_any_to_5.3.2/upgrade.sh
+++ upgrades/from_any_to_5.3.2/upgrade.sh
@@ -8,7 +8,7 @@
 NEW_PNETLAB_VERSION=5.3.2
 GREEN='\033[32m'
 NO_COLOR='\033[0m'
-URL_ZIP_FILE=https://unetlab.cloud/api/raw/?path=/UNETLAB%20I/upgrades_pnetlab/from_any_to_5.3.2/5.3.2.zip
+URL_ZIP_FILE=https://unetlab.cloud/api/raw/?path=/UNETLAB%20I/upgrades_pnetlab/extras/from_any_to_5.3.2/5.3.2.zip
 
 # Getting PNETLab version from db
 data=$(mysql -uroot -ppnetlab -D pnetlab_db -e "SELECT control_value FROM control WHERE control_value>1;" 2>/dev/null)
@@ -24,7 +24,7 @@ if [[ $pnetlab_version == *6.* ]]; then
     echo "It is not possible to upgrade to $NEW_PNETLAB_VERSION having version 6.x"
     exit 0
 fi
-    
+
 echo -e "${GREEN}Downloading $NEW_PNETLAB_VERSION zip file...${NO_COLOR}"
 wget --content-disposition -q --show-progress -O /root/$NEW_PNETLAB_VERSION.zip $URL_ZIP_FILE
 echo -e "${GREEN}$NEW_PNETLAB_VERSION zip file has been downloaded successfully${NO_COLOR}"
@@ -32,11 +32,11 @@ echo -e "${GREEN}$NEW_PNETLAB_VERSION zip file has been downloaded successfully$
 cd /root && rm -rf upgrade
 
 echo -e "${GREEN}Unzipping zip file...${NO_COLOR}"
-unzip $NEW_PNETLAB_VERSION.zip -d ./upgrade > /dev/null 2>&1
+unzip $NEW_PNETLAB_VERSION.zip -d ./upgrade >/dev/null 2>&1
 echo -e "${GREEN}Zip file has been unzipped successfully...${NO_COLOR}"
 
 chmod 755 -R upgrade
-find upgrade -type f -print0 | xargs -0 dos2unix > /dev/null 2>&1
+find upgrade -type f -print0 | xargs -0 dos2unix >/dev/null 2>&1
 
 echo -e "${GREEN}Upgrading to v$NEW_PNETLAB_VERSION...${NO_COLOR}"
 ./upgrade/upgrade
